3cde26534c42797632742ac34771e274bce334c1
Ivy <ivy@onprem.com>
2022-05-06 16:11:19 -0600

    Created a base template with nav bar.

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User
+# from forms import 
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///feedback"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "odsgodfgjdfg"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+
+connect_db(app)
+
+toolbar = DebugToolbarExtension(app)
+
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
